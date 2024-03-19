957e599b91e72d7cc91c0ff8ef15f468dc6a710e
Colin Kierans <vindexus@gmail.com>
2024-03-19 14:45:02 -0700

    Added procedures, triggers, and queries.

--- procedures.sql
+++ procedures.sql
@@ -1,3 +1,101 @@
+-- How many active loans are there?
+SELECT COUNT(*) FROM loans WHERE return_date IS NULL;
+
+-- Dynamically set the due date of a loan based on the user's types
+DROP TRIGGER IF EXISTS LoanDueDate;
+DELIMITER $$
+CREATE TRIGGER LoanDueDate 
+BEFORE INSERT ON loan
+FOR EACH ROW
+BEGIN
+    -- Student or faculty get 30 days, everyone else gets 15 days
+    SET NEW.due_date = (SELECT CASE WHEN is_student OR is_faculty THEN DATE_ADD(NOW(), INTERVAL 30 DAY) ELSE DATE_ADD(NOW(), INTERVAL 15 DAY) END
+    FROM user
+    WHERE user_id = NEW.user_id);
+
+    -- Create a new status for that copy. Keep all previous status values the same, and change is_lost and is_loaned values
+    INSERT INTO copy_status (copy_id, status_date, is_damaged, is_lost, is_reserved, is_loaned, triggering_user_id)
+    SELECT NEW.copy_id, NOW(), recent.is_damaged, FALSE, recent.is_reserved, TRUE, NEW.user_id
+    FROM copy_status recent
+    WHERE copy_id = NEW.copy_id
+    ORDER BY status_date DESC
+    LIMIT 1;
+END;
+$$
+DELIMITER ;
+
+
+-- List all media for a genre given the genre's name. Priority is given to media who have that specific genre ranked higher on their list of genres
+SELECT m.title, g.genre_name, mg.sort_order
+FROM media m
+JOIN media_genre mg
+ON mg.media_id = m.media_id
+JOIN genre g
+ON g.genre_id = mg.genre_id
+WHERE g.genre_name = "Fantasy"
+ORDER BY mg.sort_order ASC, m.title ASC;
+
+-- For Media #1, find the next reservation for it that isn't over yet
+SELECT reservation_id, user_fname, user_lname, user_email, media.title, media.media_id
+FROM media, reservation, user
+WHERE media.media_id = 1
+AND reservation.media_id = media.media_id
+AND reservation.reservation_end IS NOT NULL
+AND reservation.reservation_end < NOW()
+AND reservation.reservation_start IS NOT NULL
+ORDER BY reservation.created_date ASC
+LIMIT 1;
+
+-- How many items are out on loan right now to users who are students?
+SELECT COUNT(*) as 'Active Loans to Students'
+FROM user u, loan l
+WHERE l.user_id = u.user_id
+AND l.return_date IS NULL
+AND u.is_student = TRUE;
+
+-- Get a list all copies currently marked as damaged
+SELECT m.title, cs.*
+FROM copy_status cs
+JOIN copy c
+ON c.status_id = cs.status_id
+JOIN media m
+ON m.media_id = c.media_id
+WHERE is_damaged = 1
+ORDER BY status_date DESC;
+
+-- Returns whether a current user can check out books, based on their type, loans, and fines
+DROP PROCEDURE IF EXISTS UserCanCheckout;
+DELIMITER $$
+CREATE PROCEDURE UserCanCheckout (IN $userId INT)
+BEGIN
+    DECLARE $defaultMaxLoans INT DEFAULT 3;
+    DECLARE $studentFacultyMaxLoans INT DEFAULT 5;
+    DECLARE $maxFines DOUBLE(9,2) DEFAULT 10;
+    
+    SELECT u.user_id, u.user_fname, u.user_lname, under_loan_limit, under_fine_limit, under_loan_limit AND under_fine_limit AS 'Can Check Out'
+    FROM (
+    SELECT u.user_id, active_loans < CASE WHEN is_student OR is_faculty THEN $studentFacultyMaxLoans ELSE $defaultMaxLoans END AS under_loan_limit, COALESCE(fine_total, 0) AS 'total_unnpaid_fines', COALESCE(fine_total, 0) < $maxFines AS under_fine_limit
+    FROM user u
+    JOIN (SELECT COUNT(*) as active_loans, user_id
+    FROM loan
+    WHERE user_id = $userId
+    AND return_date IS NULL
+    GROUP BY user_id) al
+    ON al.user_id = u.user_id
+    LEFT JOIN (SELECT SUM(fine_amount) as fine_total, user_id
+    FROM fine f, loan l
+    WHERE user_id = $userId
+    AND l.loan_id = f.loan_id
+    AND f.is_paid = FALSE
+    GROUP BY user_id) ft
+    ON ft.user_id = u.user_id
+    WHERE u.user_id = $userId) limits
+    JOIN user u
+    ON u.user_id = limits.user_id;
+END $$
+DELIMITER ;
+
+-- For answering "How many items do we have?"
 DROP PROCEDURE IF EXISTS GetNumNonLostCopiesOfMedia;
 DELIMITER //
 CREATE PROCEDURE GetNumNonLostCopiesOfMedia(IN $mediaId INT)
